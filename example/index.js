9c315670c27455de5ee872dadafa69aa7adc085e
Ionică Bizău <bizauionica@gmail.com>
2015-11-23 16:52:20 +0200

    Updated the example

--- example/index.js
+++ example/index.js
@@ -1,18 +1,18 @@
 // Dependencies
-const clicons = require("../lib")
+const emojic = require("../lib")
     , colorIt = require("color-it")
     ;
 
-console.log(clicons.x + " Something went wrong");
+console.log(emojic.x + " Something went wrong");
 // => ❌ Something went wrong
 
-console.log(clicons.whiteCheckMark + " Success");
+console.log(emojic.whiteCheckMark + " Success");
 // => ✅ Success
 
 // If you want to color the things a little bit
 // ...using color-it (you can find it on github.com/IonicaBizau/node-color-it)
-console.log(" " + colorIt(clicons.x).red() + " " + colorIt("Some red error appeared").redBg())
-console.log(" " + colorIt(clicons.smiley).green() + " " + colorIt("I am a happy man!").greenBg().wetAsphalt())
+console.log(" " + colorIt(emojic.x).red() + " " + colorIt("Some red error appeared").redBg())
+console.log(" " + colorIt(emojic.smiley).green() + " " + colorIt("I am a happy man!").greenBg().wetAsphalt())
 // Note: ANSI styles will be used to color the output (see the screenshot)
 // => ❌ Some red error appeared
 // => 😃 I am a happy man

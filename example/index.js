b240fe7dd19b588526a769dd83eeeb3881fa22a1
Ionică Bizău <bizauionica@gmail.com>
2016-05-13 06:22:01 +0300

    Cleaned up and upgraded dependencies.
    Fixed the +1/-1 issue: use thumbsUp and thumbsDown to display these emojis. Fixed #4.

--- example/index.js
+++ example/index.js
@@ -11,8 +11,11 @@ console.log(emojic.whiteCheckMark + " Success");
 
 // If you want to color the things a little bit
 // ...using color-it (you can find it on github.com/IonicaBizau/node-color-it)
-console.log(" " + colorIt(emojic.x).red() + " " + colorIt("Some red error appeared").redBg())
-console.log(" " + colorIt(emojic.smiley).green() + " " + colorIt("I am a happy man!").greenBg().wetAsphalt())
+console.log(" " + colorIt(emojic.x).red() + " " + colorIt("Some red error appeared").redBg());
+console.log(" " + colorIt(emojic.smiley).green() + " " + colorIt("I am a happy man!").greenBg().wetAsphalt());
 // Note: ANSI styles will be used to color the output (see the screenshot)
 // => ❌ Some red error appeared
 // => 😃 I am a happy man
+
+console.log(" " + colorIt(emojic.thumbsUp).green() + " " + colorIt("Thumbs up!").greenBg().wetAsphalt());
+console.log(" " + colorIt(emojic.thumbsDown).green() + " " + colorIt("Thumbs down!").yellow());
